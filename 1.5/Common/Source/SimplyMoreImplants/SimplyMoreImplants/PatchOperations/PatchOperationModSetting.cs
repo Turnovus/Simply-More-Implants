9e310ecadcd4888ebedc35791105462afea13358
Turnovus <dinolover075@gmail.com>
2025-07-07 01:51:36 -0400

    Add mod settings

---
+++ 1.5/Common/Source/SimplyMoreImplants/SimplyMoreImplants/PatchOperations/PatchOperationModSetting.cs
@@ -0,0 +1,33 @@
+using System.Reflection;
+using System.Xml;
+using Verse;
+
+namespace SimplyMoreImplants.PatchOperations
+{
+    public class PatchOperationModSetting : PatchOperation
+    {
+#pragma warning disable CS0649
+        private bool invert = false;
+        private string setting;
+        private PatchOperation operation;
+#pragma warning restore CS0649
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            SimplyMoreImplantsSettings settings = LoadedModManager.GetMod<SimplyMoreImplantsMod>()
+                .GetSettings<SimplyMoreImplantsSettings>();
+            FieldInfo field =
+                typeof(SimplyMoreImplantsSettings).GetField(setting, BindingFlags.Public | BindingFlags.Instance);
+            if (field == null || field.FieldType != typeof(bool))
+                return false;
+
+            bool b = (bool)field.GetValue(settings);
+            if (invert)
+                b = !b;
+
+            if (b)
+                return operation.Apply(xml);
+            return true;
+        }
+    }
+}
