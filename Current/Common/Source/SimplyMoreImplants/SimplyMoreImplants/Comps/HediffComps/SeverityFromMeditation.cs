34e2c6429c7ab20dc10d832a38de5fe7d4aaadd9
Turnovus <dinolover075@gmail.com>
2025-07-31 15:45:44 -0400

    Create 1.6 folder

---
+++ Current/Common/Source/SimplyMoreImplants/SimplyMoreImplants/Comps/HediffComps/SeverityFromMeditation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace SimplyMoreImplants
+{
+    class HediffComp_SeverityFromMeditation : HediffComp
+    {
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            parent.Severity = Pawn.psychicEntropy.IsCurrentlyMeditating ? 1f : 0.001f;
+        }
+    }
+}
---
+++ Current/Common/Source/SimplyMoreImplants/SimplyMoreImplants/Comps/HediffComps/SleepResearch/HediffComp_SleepResearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace SimplyMoreImplants
+{
+    public class HediffComp_SleepResearch : HediffComp
+    {
+
+        private HediffCompProperties_SleepResearch Props =>
+            (HediffCompProperties_SleepResearch)props;
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+            if (Find.ResearchManager.GetProject() == null ||
+                (!Pawn.Faction.IsPlayer && !Pawn.IsPrisoner) || Pawn.Awake())
+            {
+                return;
+            }
+
+            float level =
+                Pawn.skills.GetSkill(SkillDefOf.Intellectual).levelInt;
+            float consciousness = Pawn.health.capacities.GetLevel(
+                PawnCapacityDefOf.Consciousness);
+            Find.ResearchManager.ResearchPerformed(
+                // Equation for research points per tick per skill level
+                0.08f + 0.115f * level * Props.mult * consciousness,
+                Pawn
+            );
+        }
+    }
+}
